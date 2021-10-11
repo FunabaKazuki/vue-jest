1e5cf4c3485064730720382f7251cf6d01a7f03c
kazuki <kzk.0122.wk@gmail.com>
2021-10-11 18:02:02 +0900

    a

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,23 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   setupFiles: ['<rootDir>/tests/setup.js'],    // Load Mock setting
+
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{vue,js,ts}'],
+  coverageDirectory: 'coverage',
+  
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'jest tests',
+        outputDirectory: 'reports/jest',
+        outputName: 'js-test-results.xml',
+        classNameTemplate: '{classname}-{title}',
+        titleTemplate: '{classname}-{title}',
+        ancestorSeparator: ' › ',
+      },
+    ],
+  ],
 }
